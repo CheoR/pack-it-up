f5a6bbd204d32138800fad672fb2e3b725875cf1
CheoR <cheo.roman+github@gmail.com>
2021-03-25 08:02:10 -0500

    fix: refactor code and add userId to Item object creation. Now user can see Items unassigned to boxes.

--- src/components/items/ItemList.jsx
+++ src/components/items/ItemList.jsx
@@ -1,22 +1,21 @@
 import React, { useContext, useEffect, useState } from "react"
 
 import { userStorageKey } from "../auth/authSettings"
-import { MoveContext } from "../moves/MoveProvider"
-import { BoxContext } from "../boxes/BoxProvider"
+import { UserContext } from "../users/UserProvider"
 import { ItemContext } from "./ItemProvider"
 import { ItemSummary } from "./ItemSummary"
-import "./itemList.css"
 import { Counter } from "../counter/Counter"
+import "./itemList.css"
 
 export const ItemList = () => {
 
- const { moves, getMoves } = useContext(MoveContext)
- const { boxes, getBoxes } = useContext(BoxContext)
  const { items, getItems, addItems } = useContext(ItemContext)
+ const { users, getUsers } = useContext(UserContext)
  const loggedInUserId = parseInt(sessionStorage.getItem(userStorageKey))
 
  const newItem = {
    type: {
+    userId: loggedInUserId,
     boxId: 0,
     description: "",
     value: 0,
@@ -27,33 +26,23 @@ export const ItemList = () => {
  }
 
  useEffect(() => {
-  getMoves()
-    .then(getBoxes)
+  getUsers()
     .then(getItems)
  }, []) // useEffect
  
-
-  const movesData = moves.filter(move => move.userId === loggedInUserId)
-  const boxesData = boxes.filter(box => movesData.find(move => box.moveId === move.id))
-  const itemsData = items.filter(item => boxesData.find(box => item.boxId === box.id))
-
+  const itemsData = items.filter(item => item.userId === loggedInUserId)
+  const loggedInUserObj = users.find(user => user.userId === loggedInUserId)
 
   itemsData.forEach(item => {
    /*
-    Make it easier for presentation template,
-    add properties that shows if item has a box/move assigned since
-    items/boxes can be delinked from boxes/moves, respectivly.
-
     item.hasAssociatedBox = item.boxId ? true : false
     item.hasAssociatedMove = item?.box.moveId ? true : false
     Neat way to turn number into a boolean
    */
    item.hasAssociatedBox = !!item.boxId
-   item.hasAssociatedMove = !!item?.box.moveId
+   item.hasAssociatedMove = !!item?.box?.moveId
   })
 
-  const loggedInUserObj = moves.find(move => move.userId === loggedInUserId)
-// console.log(loggedInUserObj)
    return (
     <div className="itemSummaryList">
       <h1 className="itemSummaryList__header">{ loggedInUserObj?.user.username }'s Items</h1>
