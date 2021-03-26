a9afdd2a8c977d3bb9f18ad97652ac21ec623338
CheoR <cheo.roman+github@gmail.com>
2021-03-26 03:39:20 -0500

    fix: user can create Move object and edit move name.

--- src/components/moves/MoveProvider.jsx
+++ src/components/moves/MoveProvider.jsx
@@ -16,6 +16,30 @@ export const MoveProvider = ( props ) => {
  } // getMoves
 
 
+   const addMove = ( move ) => {
+    return fetch(`${baseURL}/moves`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(move)
+    })
+    .then(getMoves)
+  } // addMove
+
+
+   const updateMove = ( move ) => {
+     return fetch(`${baseURL}/moves/${move.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(move)
+    })
+     .then(getMoves)
+  } // sendMessage
+
+
  const deleteMove = ( id ) => {
    return fetch(`${baseURL}/moves/${id}`, {
      method: "DELETE"
@@ -24,25 +48,14 @@ export const MoveProvider = ( props ) => {
  } // deleteItem
 
 
-  const addMoves = ( move ) => {
-   return fetch(`${baseURL}/moves`, {
-     method: "POST",
-     headers: {
-       "Content-Type": "application/json"
-     },
-     body: JSON.stringify(move)
-   })
-   .then(getMoves)
- } // addMove
-
-
  return (
   <MoveContext.Provider value={{
    moves,
    getMoves,
    setMoves,
-   deleteMove,
-   addMoves
+   addMove,
+   updateMove,
+   deleteMove
   }}>
    { props.children }
   </MoveContext.Provider>
