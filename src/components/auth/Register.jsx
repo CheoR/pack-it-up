837662b2c7210c9951bc1cd2ebc9ffa1669e674e
CheoR <cheo.roman+github@gmail.com>
2021-08-10 22:51:05 -0500

    layout: change register page to use material-ui.

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,16 +1,48 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+
+import { Typography, FormControl, InputLabel, Input, FormHelperText, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+
 import { authApi, userStorageKey, userStorageUserName } from './authSettings';
-import styles from './register.module.css';
 
 export const Register = () => {
+  const useStyles = makeStyles((theme) => ({
+    main: {
+      height: '75vh',
+      marginBottom: theme.spacing(2),
+    },
+    paper: {
+      position: 'relative',
+      marginTop: theme.spacing(2),
+      padding: `${theme.spacing(2)}px ${theme.spacing(1)}px`,
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      maxWidth: '80%',
+      margin: '0 auto',
+      border: '1px solid black',
+    },
+    grid: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      margin: '0 auto',
+    },
+    form: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+    },
+  }));
+
   const [registerUser, setRegisterUser] = useState({
     username: '',
     email: '',
     password: '',
   });
 
-  const [conflictDialog, setConflictDialog] = useState(false);
   const history = useHistory();
 
   const handleInputChange = (event) => {
@@ -49,41 +81,69 @@ export const Register = () => {
                 history.push('/users');
               }
             });
-        } else {
-          setConflictDialog(true);
         }
       });
   };
 
+  const classes = useStyles();
+
   return (
-    <main className={styles.registration}>
-      <dialog className={`${styles.dialog} ${styles.dialog__password}`} open={conflictDialog}>
-        <div>Account with that email address already exists</div>
-        <button type="button" className={styles.button__close} onClick={() => setConflictDialog(false)}>
-          Close
-        </button>
-      </dialog>
-      <form className={styles.registration__form__login} onSubmit={handleRegister}>
-        <h1 className={styles.registration__header}>Register for PackItUp</h1>
-        <fieldset className={styles.registration__fieldset}>
-          <label htmlFor="username">Username
-            <input type="text" name="username" id="username" className={styles.formControl} placeholder="Username" required value={registerUser.username} onChange={handleInputChange} />
-          </label>
-        </fieldset>
-        <fieldset className={styles.registration__fieldset}>
-          <label htmlFor="inputEmail"> Email address
-            <input type="email" name="email" id="email" className={styles.formControl} placeholder="Email address" required value={registerUser.email} onChange={handleInputChange} />
-          </label>
-        </fieldset>
-        <fieldset className={styles.registration__fieldset}>
-          <label htmlFor="password"> Password
-            <input type="password" name="password" id="password" className={styles.formControl} placeholder="Password" required value={registerUser.password} onChange={handleInputChange} />
-          </label>
-        </fieldset>
-        <fieldset className={styles.registration__fieldset__btn}>
-          <button className={styles.registration__btn__submit} type="submit"> Register </button>
-        </fieldset>
-      </form>
-    </main>
+    <div className={classes.main}>
+      <Paper className={classes.paper}>
+        <Typography varient="h1" component="h1">
+          Register
+        </Typography>
+        <form onSubmit={handleRegister} className={classes.form}>
+          <Grid container className={classes.grid}>
+            <Grid item>
+              <FormControl>
+                <InputLabel htmlFor="username">Username</InputLabel>
+                <Input
+                  type="text"
+                  name="username"
+                  id="username"
+                  aria-describedby="Create Username"
+                  onChange={handleInputChange}
+                />
+                <FormHelperText id="username">Create Username</FormHelperText>
+              </FormControl>
+            </Grid>
+            <Grid item>
+              <FormControl>
+                <InputLabel htmlFor="email">Email address</InputLabel>
+                <Input
+                  type="email"
+                  name="email"
+                  id="email"
+                  aria-describedby="Your Email"
+                  onChange={handleInputChange}
+                />
+                <FormHelperText id="email">Email Address</FormHelperText>
+              </FormControl>
+            </Grid>
+            <Grid item>
+              <FormControl>
+                <InputLabel htmlFor="password">Password</InputLabel>
+                <Input
+                  type="password"
+                  name="password"
+                  id="password"
+                  aria-describedby="Password"
+                  onChange={handleInputChange}
+                />
+                <FormHelperText id="password">Password</FormHelperText>
+              </FormControl>
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
+            Register
+          </Button>
+        </form>
+      </Paper>
+    </div>
   );
 };
